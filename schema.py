be9c1822fc88fac03043dc13e415833c77e29de9
zhaofeng-shu33 <616545598@qq.com>
2020-11-11 23:25:25 +0800

    format data

--- schema.py
+++ schema.py
@@ -19,10 +19,10 @@ def _generate_three_circle_data():
     rd = random.Random()
     # make the result reproducible across multiple run
     rd.seed(0)
-    for i in range(1,4): # radius: 0.1*i
-        for j in range(num_list[i-1]):
-            r = 0.1*i + 0.01 * (2*rd.random()-1)
-            angle = 2*np.pi * rd.random()
+    for i in range(1, 4): # radius: 0.1 * i
+        for _ in range(num_list[i - 1]):
+            r = 0.1 * i + 0.01 * (2 * rd.random() - 1)
+            angle = 2 * np.pi * rd.random()
             pos_list.append([r * np.cos(angle), r * np.sin(angle)])
             ground_truth.append(i)
     return (np.asarray(pos_list), np.asarray(ground_truth))
